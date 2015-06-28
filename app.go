a1049a912c4296001ab0b85bcbcf7abd7b6225ed
Ricardo Rossi <rrossi@endata.com>
2015-06-28 13:44:10 -0500

    initial work on router and route provider

--- app.go
+++ app.go
@@ -2,7 +2,20 @@ package main
 
 import "github.com/gopherlabs/gopher"
 
+type WebContext struct{}
+
 func main() {
 	gopher.Start()
 	gopher.Hello("App")
+	router := gopher.NewRouter(WebContext{})
+	_ = router
+	//router.Get("/", (*WebContext).Home)
 }
+
+//func (c *WebContext) Home(rw web.ResponseWriter, req *web.Request) {
+//	if c.User != nil {
+//		fmt.Fprint(rw, "Hello,", c.User.Name)
+//	} else {
+//		fmt.Fprint(rw, "Hello, anonymous person")
+//	}
+//}
