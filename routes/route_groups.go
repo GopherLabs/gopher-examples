aeeffbb2e6bba769e0b9144ef35394a15820ea7d
Ricardo Rossi <rrossi@endata.com>
2015-07-12 18:09:55 -0500

    Added auto-initialization

--- routes/route_groups.go
+++ routes/route_groups.go
@@ -9,24 +9,16 @@ import (
 
 func main() {
 
-	g.Initialize()
+	g.Context().Set("user", "Ricardo Rossi")
 
-	g.Context.Set("user", "Ricardo Rossi")
-
-	g.Router.Get("/hello", func(rw http.ResponseWriter, req *http.Request) {
-		g.Log.Info("Now we are cooking!")
-		fmt.Fprintln(rw, "Hello, "+g.Context.Get("user").(string))
+	g.Router().Get("/hello", func(rw http.ResponseWriter, req *http.Request) {
+		g.Log().Info("Now we are cooking!")
+		fmt.Fprintln(rw, "Hello, "+g.Context().Get("user").(string))
 	})
 
-	g.Router.NotFound(func(rw http.ResponseWriter, req *http.Request) {
+	g.Router().NotFound(func(rw http.ResponseWriter, req *http.Request) {
 		fmt.Fprint(rw, "hey! Could not find this page")
 	})
-
-	//app.Routes().Get
-	//	router.Get("/", func(rw http.ResponseWriter, req *http.Request) {
-	//		fmt.Fprintln(rw, "Hello, Gopher!")
-	//	})
-	//	router.Serve()
 
 	g.ListenAndServe()
 }
