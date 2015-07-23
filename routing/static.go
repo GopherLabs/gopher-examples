8d9afc38d8d8f7448adad75c9b82188f64145b61
Ricardo Rossi <rrossi@endata.com>
2015-07-23 14:35:29 -0500

    Added Static file serving API Route.Static

--- routing/static.go
+++ routing/static.go
@@ -1,16 +1,31 @@
 package main
 
 import (
-	"net/http"
 	// Only for the purpose of syntax clarity, we are using dot import notation below.
 	// It should be noted, however, that the Go team does not recommend using the dot
 	// import since it can cause some odd behaviour in certain cases.
+	"net/http"
+
 	. "github.com/gopherlabs/gopher"
 )
 
 func main() {
+	//	App.Config(Config{
+	//		KEY_ROUTER: ConfigRouter{
+	//			Port: 3000,
+	//			Host: "0.0.0.0",
+	//			StaticDirs: map[string]string{
+	//				"/static": "./static/",
+	//			},
+	//		},
+	//	})
+
+	Route.Static("/static", "./static")
+	Route.Static("/", "./public")
+
 	Route.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		Render.Text(w, "Hello, Gopher!")
 	})
+
 	ListenAndServe()
 }
