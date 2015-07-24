082688397a66cee7839126807523aa547f9454a9
Ricardo Rossi <rrossi@endata.com>
2015-07-24 14:01:46 -0500

    Added GetHttpHandler() for standard support

---
+++ routing/http_handler.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"net/http"
+	// Only for the purpose of syntax clarity, we are using dot import notation below.
+	// It should be noted, however, that the Go team does not recommend using the dot
+	// import since it can cause some odd behaviour in certain cases.
+	. "github.com/gopherlabs/gopher"
+)
+
+func main() {
+	Route.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		Render.Text(w, "Hello, Gopher!")
+	})
+
+	http.ListenAndServe("localhost:8080", GetHttpHandler())
+}
